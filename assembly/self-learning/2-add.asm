420ea730469b41a8ab6fe07cc367b382f87b1c24
0kraven <kabeercybersec@gmail.com>
2025-03-01 19:31:05 +0500

    assembly

---
+++ assembly/self-learning/2-add.asm
@@ -0,0 +1,15 @@
+.386
+.model flat, stdcall
+.stack 4096
+
+include kernel32.inc
+includelib kernel32.lib
+
+.code
+main PROC
+    mov EAX, 5          ; Load first operand
+    add EAX, 10         ; Perform addition (5 + 10)
+    
+    invoke ExitProcess, 0  ; Exit program cleanly
+main ENDP
+END main
--- assembly/self-learning/make.bat
+++ assembly/self-learning/make.bat
@@ -3,7 +3,6 @@ set INCLUDE=C:\masm32\include
 set LIB=C:\masm32\lib
 echo ################# make file ################
 if "%1"=="" (
-    pause
     exit /b 1
 )
 echo [+] assembling the source code.
@@ -11,7 +10,6 @@ ml /c /coff %1.asm
 
 if errorlevel 1 (
     echo Assembly failed! Check your code.
-    pause
     exit /b 1
     
 )
@@ -20,7 +18,6 @@ link /subsystem:console /LIBPATH:"C:\masm32\lib" %1.obj
 echo [+] linking the object files.
 if errorlevel 1 (
     echo Linking failed!
-    pause
     exit /b 1
 )
 
